4aa1aa01cecdf68338438591b62067f6fd4c7861
Scott Nath <github@scottnath.com>
2015-04-20 13:00:05 -0400

    pattern re-aranging

--- patterns/components/figure-image/test/main.js
+++ patterns/components/figure-image/test/main.js
@@ -11,15 +11,15 @@ describe('pattern-library-figure-image', function() {
   describe('files-exist', function() {
 		var meta = yaml.safeLoad(fs.readFileSync(patternYml, 'utf8'));
 
-	    if (meta.css) {
-				it('should contain css file', function() {
-			    should.exist(meta.css);
+	    if (meta.sass) {
+				it('should contain sass file', function() {
+			    should.exist(meta.sass);
 				});
 			}
 
-	    if (meta.sass) {
-				it('should contain a sass file', function() {
-			    should.exist(meta.sass);
+	    if (meta.twig) {
+				it('should contain a twig file', function() {
+			    should.exist(meta.twig);
 				});
 			}
 
@@ -29,9 +29,9 @@ describe('pattern-library-figure-image', function() {
 				});
 			}
 			
-	    if (meta.html) {
-				it('should contain an html pattern', function() {
-			    should.exist(meta.html);
+	    if (meta.includes) {
+				it('should include an existing pattern', function() {
+			    should.exist(path.join(meta.includes[0],'pattern.yml'));
 				});
 			}
 	});
